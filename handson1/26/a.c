dfd1279dcd45e7f9004060ae9fb45bc30cfccc0d
t-sameer <76210561+t-sameer@users.noreply.github.com>
2025-09-20 02:22:07 +0530

    delete

--- handson1/26/a.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-        printf("Argument passed is:  %s!\n", argv[1]);
-    return 0;
-}
-
--- handson1/30/hello.sh
+++
@@ -1,1 +0,0 @@
-echo "Hello this script ran at $(date)" >> /home/sameer/Study/Systems/handsOn/30/log.txt
